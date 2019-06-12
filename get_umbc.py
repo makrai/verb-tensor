f727b70424955fba12a43ed2537f05678595de46
Márton Makrai <makrai.nlp@gmail.com>
2019-06-12 13:40:35 +0200

    file handling

--- get_umbc.py
+++ get_umbc.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# In[ ]:
 
 
 from collections import defaultdict
+import glob
 import itertools
 import os
 import pandas as pd
@@ -18,13 +26,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s [%(lineno)d] %(message)s')
 
 
+# In[ ]:
+
+
+proj_dir = '/mnt/store/home/makrai/project/verb-tensor/'
+
 def get_umbc_dict():
+    dict_filen = os.path.join(proj_dir, 'umbc_freq.pkl')
+    if os.path.exists(dict_filen):
+        return pickle.load(open(dict_filen, mode='rb'))
     umbc_dir = '/mnt/store/home/makrai/data/language/english/corp/umbc_WebBase/English/'
     freq = defaultdict(int)
-    for filen in os.listdir(umbc_dir):
+    for filen in glob.glob(os.path.join(umbc_dir, 'en-common_crawl-*.conllu.xz')):
         logging.info(filen)
-        for i, sentence in  enumerate(parse_incr(lzma.open(os.path.join(umbc_dir, filen), mode='rt',
-                                                           encoding="utf-8"))):
+        for i, sentence in  enumerate(parse_incr(lzma.open(os.path.join(
+                umbc_dir, filen), mode='rt', encoding="utf-8"))):
             if not i % 100000:
                 logging.debug(i)
             root = sentence.to_tree()
@@ -42,13 +58,37 @@ def get_umbc_dict():
                     obj = child.token['lemma']
             #if bool(obj) and bool(subj):
             freq[(subj, root.token['lemma'], obj)] += 1
-        #pickle.dump(freq, open('/mnt/store/home/makrai/project/verb-tensor/umbc_freq.pkl', mode='wb'))
+        pickle.dump(freq, open(dict_filen, mode='wb'))
     return freq
 
 
+# In[ ]:
+
+
 def get_umbc_df():
-    freq = pickle.load(open('/mnt/store/home/makrai/project/verb-tensor/umbc_freq.pkl', mode='rb'))
+    freq = get_umbc_dict()
     freq_df = pd.DataFrame.from_records(list(freq.items()), columns=['svo', 'freq'])
-    freq_df[['subj', 'verb', 'obj']] = pd.DataFrame(freq_df.svo.tolist(), index=freq_df.index)                                                                                                                       
+    freq_df[['subj', 'verb', 'obj']] = pd.DataFrame(freq_df.svo.tolist(),
+            index=freq_df.index)
     del freq_df['svo']
+    df.sort_values('freq', ascending=False).to_csv(os.path.join(proj_dir, 'umbc_freq.csv'), sep='\t', index=False)
     return freq_df
+
+
+# In[ ]:
+
+
+df = get_umbc_df()
+
+
+# In[ ]:
+
+
+df[(df.subj != '') & (df.obj != '')].sort_values('freq', ascending=False).head()
+
+
+# In[ ]:
+
+
+df = get_umbc_df()
+
