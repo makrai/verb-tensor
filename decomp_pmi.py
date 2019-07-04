def141188490f6e7a0bc8e89a34958acaa4dad89
makrai <makraimarton@gmail.com>
2019-07-04 20:07:54 +0200

    Debugged; argument passing changed

--- decomp_pmi.py
+++ decomp_pmi.py
@@ -21,13 +21,15 @@ logging.basicConfig(level=logging.DEBUG,
         format='%(levelname)-8s [%(lineno)d] %(message)s')
 
 class VerbTensor():
-    def __init__(self, cutoff=1):
+    def __init__(self, weight, cutoff, rank):
+        self.weight = weight
         self.cutoff = cutoff
+        self.rank = rank
         self.project_dir = '/mnt/store/home/makrai/project/verb-tensor/'
         self.tensor_dir = os.path.join(self.project_dir, 'depCC')
         self.assoc_df_filen_patt = os.path.join(self.project_dir,
-                                                'dataframe/depCC/assoc00.{}')
-        self.modes = ['nsubj, ROOT, dobj']
+                                                'dataframe/depCC/assoc0.{}')
+        self.modes = ['nsubj', 'ROOT', 'dobj']
         # mazsola: ['NOM', 'stem', 'ACC']
 
     def append_pmi(self, svo_count=None, debug_index=None):
@@ -38,8 +40,8 @@ class VerbTensor():
         """
         if svo_count is None:
             filen = os.path.join(self.project_dir,
-                                 'dataframe/depCC/freq00.tsv')
-            svo_count = df.read_csv(filen, sep='\t')
+                                 'dataframe/depCC/freq0.tsv')
+            svo_count = pd.read_csv(filen, sep='\t', keep_default_na=False)
         logging.info('Computing marginals..')
         marginal = {mode: svo_count.groupby(mode).sum() for mode in self.modes}
         logging.info('Computing 2-marginals..')
@@ -55,34 +57,22 @@ class VerbTensor():
                     rsuffix='_{}'.format(mode_pair))
         svo_count['dice'] = 3*svo_count.freq # This is only the numerator.
         svo_count['freq2'] = svo_count.freq
-        if debug_index:
-            logging.debug(svo_count.loc[debug_index])
         svo_count['dice_denom'] = 0
         for mode in self.modes:
             svo_count.dice_denom += svo_count['freq_{}'.format(mode)]
-            if debug_index:
-                logging.debug(svo_count.loc[debug_index])
         svo_count.dice /= svo_count.dice_denom
         del svo_count['dice_denom']
-        if debug_index:
-            logging.debug(svo_count.loc[debug_index])
         logging.info('Computing PMI variants..')
         for name in svo_count.columns[4:]:
             if name != 'dice':
                 svo_count[name] = np.log2(svo_count[name]) - log_total
         svo_count['pmi'] = svo_count.freq2
         svo_count['iact_info'] = -svo_count.freq2
-        if debug_index:
-            logging.debug(svo_count.loc[debug_index])
         for mode in self.modes:
             svo_count.pmi -= svo_count['freq_{}'.format(mode)]
             svo_count.iact_info += svo_count['freq_{}'.format(mode)]
-            if debug_index:
-                logging.debug(svo_count.loc[debug_index])
         for mode_pair in itertools.combinations(self.modes, 2):
             svo_count.iact_info -= svo_count['freq_{}'.format(mode_pair)]
-            if debug_index:
-                logging.debug(svo_count.loc[debug_index])
         svo_count.pmi = np.max(svo_count.pmi, 0)
         svo_count.iact_info = np.max(svo_count.iact_info, 0) 
         # TODO Interpretation of positive pointwise interaction information
@@ -95,7 +85,7 @@ class VerbTensor():
                          index=False, float_format='%.5g')
         return svo_count, log_total
 
-    def get_sparse(self, weight):
+    def get_sparse(self):
         if os.path.exists(self.sparse_filen):
             logging.info('Loading tensor..')
             self.sparse_tensor, self.index =  pickle.load(open(
@@ -108,57 +98,67 @@ class VerbTensor():
                 self.assoc_df_filen_patt.format('pkl'))
         else:
             self.pmi_df, log_total = self.append_pmi()
-        df = self.pmi_df[self.pmi_df.freq>self.cutoff].copy(), # TODO ?
-        logging.debug(weight)
-        if weight == 'freq': # TODO Is this needed?
-            df[weight] = np.log(df[weight] + 1)
+        #logging.debug('')
+        df = self.pmi_df[self.pmi_df.freq>self.cutoff].copy() # TODO ?
+        if self.weight == 'freq': # TODO Is this needed?
+            df[self.weight] = np.log(df[self.weight] + 1)
+        logging.info('Preparing the index.. (weight={})'.format(self.weight))
         self.index = {
-            mode: bidict(df.groupby(mode)[weight].sum().argsort().to_dict())
+            mode: 
+            bidict(df.groupby(mode)[self.weight].sum().argsort().to_dict())
             for mode in self.modes}
         for mode in self.modes:
+            # Adding nan as an argument filler to the index..
             self.index[mode][np.nan] = len(self.index[mode])
+            # Inserting a column for index..
             df['{}_i'.format(mode)] = df[mode].apply(self.index[mode].get)
         logging.debug('Creating tensor..')
         coords = df[['{}_i'.format(mode)
                      for mode in self.modes]].T.to_records(index=False)
+        logging.debug('')
         coords = tuple(map(list, coords))
-        data = df[weight].values
+        logging.debug('')
+        data = df[self.weight].values
+        logging.debug('')
         shape=tuple(len(self.index[mode]) for mode in self.modes)
-        logging.debug(([len(y) for y in coords], len(data)))
         logging.info(shape)
         self.sparse_tensor = sktensor.sptensor(coords, data, shape=shape)
         pickle.dump((self.sparse_tensor, self.index), open(os.path.join(
             self.tensor_dir, self.sparse_filen), mode='wb'))
 
-    def decomp(self, weight, rank):
-        logging.info((weight, rank, self.cutoff))
+    def decomp(self):
+        logging.info((self.weight, self.rank, self.cutoff))
         decomp_filen = os.path.join(
             self.tensor_dir, 
-            '{}_{}_{}_{}.pkl').format( 'ktensor', weight, self.cutoff, rank)
+            '{}_{}_{}_{}.pkl').format('ktensor', self.weight, self.cutoff,
+                                      self.rank)
         if os.path.exists(decomp_filen):
             logging.warning('File exists')
             return
         self.sparse_filen = os.path.join(
             self.tensor_dir, 
-            '{}_{}_{}.pkl'.format( 'sparstensr', weight, self.cutoff))
-        self.get_sparse(weight)
-        result = orth_als(self.sparse_tensor, rank)
+            '{}_{}_{}.pkl'.format( 'sparstensr', self.weight, self.cutoff))
+        self.get_sparse()
+        result = orth_als(self.sparse_tensor, self.rank)
         pickle.dump(result, open(decomp_filen, mode='wb'))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Decompose a tensor of verb and argument cooccurrences')
-    parser.add_argument('--weight', default='freq')
-    parser.add_argument('--cutoff', type=int, default=0) # TODO ?
+    parser.add_argument(
+        '--weight', default='freq', 
+        help="['freq', 'pmi', 'iact_info', 'salience', 'iact_sali', 'dice']")
+    parser.add_argument('--cutoff', type=int, default=0) # TODO Fit to memory!
     parser.add_argument('--rank', type=int, default=100)
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    decomposer = VerbTensor(cutoff=args.cutoff)
-    #for weight in ['freq', 'pmi', 'iact_info', 'salience', 'iact_sali', 'dice']:
+    decomposer = VerbTensor(weight=args.weight, cutoff=args.cutoff,
+                            rank=args.rank)
+    #for weight in 
     #try:
-    decomposer.decomp(args.weight, args.rank)
+    decomposer.decomp()
     #except Exception as e:
     #logging.warning(e)
