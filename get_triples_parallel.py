26fb431ee5f86ea5d7a3b7df0e472fccd189d20e
makrai <makraimarton@gmail.com>
2019-09-02 18:17:32 +0200

    Hack: 0th df originally has nsubj.. in index (not regular column)

--- get_triples_parallel.py
+++ get_triples_parallel.py
@@ -16,12 +16,12 @@ logging.basicConfig(level=logging.INFO,
 df_dict = '/mnt/permanent/home/makrai/project/verb-tensor/top_level/dataframe/'
 
 def sum_frames(trailing_zeros='00'):
-    logging.info('Reading dataframes..')
-    dfs = [pd.read_pickle(os.path.join(df_dict, 'freq{}{}.pkl').format(i, trailing_zeros)).set_index(
-        ['nsubj', 'ROOT', 'dobj']) 
-           for i in range(10)]
-    logging.info('Summing dataframes..')
-    df = reduce(lambda x, y: x.add(y, fill_value=0), dfs)
+    dfs = pd.read_pickle(os.path.join(df_dict, 'freq0{}.pkl').format(
+        trailing_zeros)).reset_index()
+    for i in range(1, 10):
+        logging.info(i)
+        df =+ pd.read_pickle(os.path.join(df_dict, 'freq{}{}.pkl').format(
+            i, trailing_zeros)).set_index( ['nsubj', 'ROOT', 'dobj'])
     logging.info('Pickling dataframes..')
     df = df.astype(int)
     df = df.sort_values('freq', ascending=False)
