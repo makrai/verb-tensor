a5940bd0414b6f6be350e31725b7b63751d3041a
makrai <makraimarton@gmail.com>
2019-09-10 17:19:40 +0200

    fill_value restored

--- get_triples_parallel.py
+++ get_triples_parallel.py
@@ -9,23 +9,26 @@ import sys
 import pandas as pd
 
 import logging
-logging.basicConfig(level=logging.INFO, 
+logging.basicConfig(level=logging.DEBUG, 
                     format='%(levelname)-8s [%(lineno)d] %(message)s')
 
 
-df_dict = '/mnt/permanent/home/makrai/project/verb-tensor/top_level/dataframe/'
+df_dir = '/mnt/permanent/home/makrai/project/verb-tensor/top_level/dataframe/'
 
 def sum_frames(trailing_zeros='00'):
-    dfs = pd.read_pickle(os.path.join(df_dict, 'freq0{}.pkl').format(
-        trailing_zeros)).reset_index()
+    filen = os.path.join(df_dir, 'freq0{}.pkl').format(trailing_zeros)
+    logging.info(filen)
+    df = pd.read_pickle(filen).reset_index().set_index(['nsubj', 'ROOT', 'dobj'])#
+    logging.debug(df.head(1))
     for i in range(1, 10):
-        logging.info(i)
-        df =+ pd.read_pickle(os.path.join(df_dict, 'freq{}{}.pkl').format(
-            i, trailing_zeros)).set_index( ['nsubj', 'ROOT', 'dobj'])
+        filen = os.path.join(df_dir, 'freq{}{}.pkl').format(i, trailing_zeros)
+        logging.info(filen)
+        df0 = pd.read_pickle(filen).reset_index().set_index(['nsubj', 'ROOT', 'dobj'])
+        df += df0.reindex(df.index, fill_value=0)
     logging.info('Pickling dataframes..')
     df = df.astype(int)
     df = df.sort_values('freq', ascending=False)
-    df.to_pickle(os.path.join(df_dict, 'freq{}.pkl').format(trailing_zeros))
+    df.to_pickle(os.path.join(df_dir, 'freq{}.pkl').format(trailing_zeros))
 
 
 if __name__ == '__main__':
