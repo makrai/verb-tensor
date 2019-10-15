378db928819da9ae819053ee935ac7aa84b60be8
makrai <makraimarton@gmail.com>
2019-10-15 17:36:26 +0200

    Logging

--- sum_triple_count_dfs.py
+++ sum_triple_count_dfs.py
@@ -17,14 +17,14 @@ df_dir = '/mnt/permanent/home/makrai/project/verb-tensor/verb/dataframe/'
 
 def sum_frames(common_suff):
     filen = os.path.join(df_dir, 'freq0{}.pkl').format(common_suff)
-    logging.info(filen)
+    logging.info('Reading {}..'.format(filen))
     df = pd.read_pickle(filen).reset_index().set_index(['nsubj', 'ROOT', 'dobj'])
     for i in range(1, 10):
         filen = os.path.join(df_dir, 'freq{}{}.pkl').format(i, common_suff)
         if not os.path.isfile(filen):
             logging.warning('File {} not exists.'.format(filen))
             continue
-        logging.info(filen)
+        logging.info('Reading {}..'.format(filen))
         df0 = pd.read_pickle(filen).reset_index().set_index(['nsubj', 'ROOT', 'dobj'])
         df += df0.reindex(df.index, fill_value=0)
     logging.info('Pickling dataframes..')
