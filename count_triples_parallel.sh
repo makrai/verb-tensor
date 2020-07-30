10fe6fe1ea7d04f3b341aedc839e6d80e4990c34
makrai <makraimarton@gmail.com>
2020-07-30 17:50:04 +0200

    pyconll, 100 threads

--- count_triples_parallel.sh
+++ count_triples_parallel.sh
@@ -1,7 +1,7 @@
 for j in 0 `seq 9`
 do 
-  for part in 0 `seq 9`
+  for part in 00 `seq -w 99`
   do 
-    time nice python count_triples_single_thread.py $j${part}
+    time nice python count_triples_pyconll.py $j${part}
   done & 
 done
