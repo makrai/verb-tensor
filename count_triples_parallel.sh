c154a3d95cb4acdbc274884c342c7fe30cb7b378
makrai <makraimarton@gmail.com>
2020-07-31 17:22:01 +0200

    Fixes order of cycles

--- count_triples_parallel.sh
+++ count_triples_parallel.sh
@@ -1,7 +1,10 @@
-for j in 0 `seq 9`
+for i in 0 `seq 9`
 do 
-  for part in 00 `seq -w 99`
+  for k in 0 `seq 9`
   do 
-    time nice python count_triples_pyconll.py $j${part}
-  done & 
+    for j in 0 `seq 9`
+    do 
+      time nice python count_triples_pyconll.py $i$j$k
+    done 
+  done &
 done
