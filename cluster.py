1baf37204bb2b547faf2405ecf78f754782c7a06
Márton Makrai <makrai.nlp@gmail.com>
2020-08-27 16:01:14 +0200

    Updating in progress

--- cluster.py
+++ cluster.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from collections import defaultdict
+import configparser 
 from itertools import groupby
 import operator
 import os
@@ -11,7 +12,6 @@ import numpy as np
 import sklearn
 import pickle
 import random
-import sparse
 from sklearn.manifold import TSNE 
 import sktensor
 import umap
@@ -22,20 +22,16 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)-8s [%(lineno)d] %(message)s')
 
-if not os.path.exists('cp_orth.py'):
-    wget.download('http://web.stanford.edu/~vsharan/cp_orth.py')
-from cp_orth import orth_als
-
 
 class ClusterVerbs():
-    def __init__(self, weight='freq', cutoff=2, rank=32, clusser_dim=10,
-                 min_cluster_size=5, min_samples=None, metric='cosine',
-                 init='spectral'):
+    def __init__(self, weight='log_freq', cutoff=100, rank=256, clusser_dim=16,
+                 min_cluster_size=5, min_samples=None, metric='cosine'):
         self.clusser_dim = clusser_dim
         self.min_cluster_size = min_cluster_size
         self.min_samples = min_samples
         self.metric = metric
-        self.init = init
+        config = configparser.ConfigParser()
+        config.read('config.ini')
         tensor_dir = config['DEFAULT']['ProjectDirectory']+'tensor/0'
         logging.info('Loading tensor and index..')
         self.ktensor, _, _, _ = pickle.load(open(os.path.join(
@@ -49,10 +45,8 @@ class ClusterVerbs():
     def do_umap(self, n_components):
         if self.ktensor.U[1].shape[1] <= n_components:
             return self.ktensor.U[1]
-        #mapping = umap.UMAP(n_components=n_components, metric=self.metric,
-        mapping = TSNE(n_components=n_components, metric=self.metric,
-                       init=self.init, 
-                       method='barnes_hut' if n_components > 4 else 'exact')
+        mapping = umap.UMAP(n_components=n_components, metric=self.metric)
+        # Further kwargs: n_neighbors=15
         return mapping.fit_transform(self.ktensor.U[1])
 
     def plot_cluster(self):
