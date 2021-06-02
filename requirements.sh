02c1d44b6dd17da966b70c8d07bafcff3047b6b2
makrai <makraimarton@gmail.com>
2021-06-02 20:48:16 +0200

    sparse

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #apt install gcc
 conda install configparser matplotlib pandas scipy urllib3 
-conda install -c conda-forge hdbscan umap-learn bidict 
+conda install -c conda-forge hdbscan umap-learn bidict sparse
 conda install -c anaconda cython
 conda install 'numpy>=1.17'
 pip install scikit-tensor-py3 pyconll 
