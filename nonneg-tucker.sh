fb7366b264c80ed646eb5fecbbb3066c7a649b87
makrai <makraimarton@gmail.com>
2021-10-08 14:31:39 +0200

    nonneg-tucker.sh

---
+++ nonneg-tucker.sh
@@ -0,0 +1,7 @@
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 64 --non-empty --cutoff 1000000
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 32 --non-empty --cutoff 1000000
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 128 --non-empty --cutoff 1000000
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 64 --cutoff 1000000
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 32 --non-empty --cutoff 3000000
+date; time nice python decomp_pmi.py --non-negative --weight npmi --rank 64 --non-empty --cutoff 300000
+date; time nice python decomp_pmi.py --non-negative --weight for --rank 64 --non-empty --cutoff 1000000
