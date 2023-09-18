f5b6d34094d3e0f00c579c2634602d38511911fb
Márton Makrai <makrai.hlt@gmail.com>
2023-09-18 15:39:50 +0200

    pep8

--- eval_tensor.py
+++ eval_tensor.py
@@ -11,7 +11,6 @@ import pandas as pd
 import pickle
 import tensorly as tl
 
-import bidict
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -28,6 +27,7 @@ def read_ks():
     cols = ['subject1', 'verb1', 'object1', 'subject2', 'verb2', 'object2']
     return read_sim_data('emnlp2013_turk.txt').groupby(cols).mean().drop(
         columns=['annotator'])
+
 
 def read_gs():
     cols = ['verb', 'subject', 'object', 'landmark', 'hilo']
@@ -62,22 +62,22 @@ class VerbTensorEvaluator():
 
     def get_cols(self):
         if self.mode_to_test == 'svo':
-            #logging.info('Assuming df is Kartsaklis and Sadrzadeh Turk')
+            # logging.info('Assuming df is Kartsaklis and Sadrzadeh Turk')
             self.query1_cols = list(enumerate(['subject1', 'verb1', 'object1']))
             self.query2_cols = list(enumerate(['subject2', 'verb2', 'object2']))
             self.sim_col = 'score'
         elif self.mode_to_test == 'ROOT':
-            #logging.info('Assuming df is SimVerb')
+            # logging.info('Assuming df is SimVerb')
             self.query1_cols = [(1, 'verb1')]
             self.query2_cols = [(1, 'verb2')]
             self.sim_col = 'sim'
         elif self.mode_to_test == 'nsubj':
-            #logging.info('Assuming df is SimLex')
+            # logging.info('Assuming df is SimLex')
             self.query1_cols = [(0, 'word1')]
             self.query2_cols = [(0, 'word2')]
             self.sim_col = 'SimLex999'
         elif self.mode_to_test == 'dobj':
-            #logging.info('Assuming df is SimLex')
+            # logging.info('Assuming df is SimLex')
             self.query1_cols = [(2, 'word1')]
             self.query2_cols = [(2, 'word2')]
             self.sim_col = 'SimLex999'
@@ -93,7 +93,8 @@ class VerbTensorEvaluator():
                                              mode='rb'))
         factors = self.decomped_tns.factors
         logging.debug(self.include_empty)
-        _, self.index = pickle.load(open(os.path.join(tensor_dir,
+        _, self.index = pickle.load(open(os.path.join(
+            tensor_dir,
             f'sparstensr_{self.weight}_{include_empty_str}_{self.cutoff}.pkl'), mode='rb'))
         if self.decomp_algo == 'parafac':
             factors = [factor.todense() for factor in factors]
@@ -102,12 +103,13 @@ class VerbTensorEvaluator():
                                                  self.decomped_tns.weights.todense()))
             for mode_i in range(3):
                 factors[mode_i] *= sq_lam
-        if self.normlz_vocb and self.mode_to_test!='svo':
+        if self.normlz_vocb and self.mode_to_test != 'svo':
             mode_i = self.query1_cols[0][0]
             factors[mode_i] /= np.apply_along_axis(
-                np.linalg.norm, 1, factors[mode_i]).reshape((-1,1))
+                np.linalg.norm, 1, factors[mode_i]).reshape((-1, 1))
         modes = ['nsubj', 'ROOT', 'dobj']
         self.oov = defaultdict(int)
+
         def lookup(word, mode_i=1):
             if word in self.index[modes[mode_i]]:
                 return factors[mode_i][self.index[modes[mode_i]][word]]
@@ -117,9 +119,8 @@ class VerbTensorEvaluator():
         self.lookup = lookup
 
     def test_sim(self, task_df0):
-        task_df = task_df0.copy() # Subj and obj sim not to go in the same df
+        task_df = task_df0.copy()  # Subj and obj sim not to go in the same df
         self.get_cols()
-        mean = task_df[self.sim_col].mean()
         self.load_embeddings()
         for mode_i, query_w_col in self.query1_cols + self.query2_cols:
             series = task_df[query_w_col].apply(
@@ -138,9 +139,10 @@ class VerbTensorEvaluator():
         else:
             query_v_cols = [f'{item[0][1]}_v'
                             for item in [self.query1_cols, self.query2_cols]]
+
         def cell_dot_cell(series):
             return series[0].dot(series[1])
-        target_col = 'tensor_sim'#_{self.weight}_{self.cutoff}_{self.rank}'
+        target_col = 'tensor_sim'  # _{self.weight}_{self.cutoff}_{self.rank}'
         task_df[target_col] = task_df[query_v_cols].apply(cell_dot_cell, axis=1)
         top_oov = sorted(self.oov.items(), key=operator.itemgetter(1), reverse=True)[:5]
         logging.debug(f'OOV: {top_oov}')
@@ -149,7 +151,7 @@ class VerbTensorEvaluator():
         return sim_corr[target_col]
 
     def predict_verb(self, target_df, prec_at=1, logg_oov=False,
-        target_pref='verb', cols_suff='', majority_baseline=5):
+                     target_pref='verb', cols_suff='', majority_baseline=5):
         """
         Results are bellow the majority baseline.
         cols_suff: GS11 or Jenatton: '', KS13: 1 or 2
@@ -157,23 +159,25 @@ class VerbTensorEvaluator():
         target_pref: GS11: verb or landmark, KS13: verb
         """
         self.load_embeddings()
-        #logging.debug('Making predictions..')
+        # logging.debug('Making predictions..')
         low_verb_low = tl.tenalg.mode_dot(
                 self.decomped_tns.core, self.decomped_tns.factors[1], mode=1)
+
         def verb_pred(series):
             series = series[[f'subject{cols_suff}', f'object{cols_suff}']]
             v_subj = self.lookup(series[0], mode_i=0)
             v_obj = self.lookup(series[1], mode_i=2)
             assocv = tl.tenalg.mode_dot(low_verb_low, v_subj, mode=0).dot(v_obj)
             predicted_ids = np.argsort(-assocv)
-            return [self.index['ROOT'].inverse[cols_suff] 
+            return [self.index['ROOT'].inverse[cols_suff]
                     for cols_suff in predicted_ids[:prec_at]]
         target_df['predicted'] = target_df.apply(verb_pred, axis=1)
         if logg_oov:
             logging.debug(sorted(self.oov.items(), key=operator.itemgetter(1),
                                  reverse=True))
-        #logging.debug('Evaluating predictions..')
-        target = f'{target_pref}{cols_suff}' 
+        # logging.debug('Evaluating predictions..')
+        target = f'{target_pref}{cols_suff}'
+
         def is_good(series):
             return series[target] in series['predicted']
         target_df[f'good'] = target_df.apply(is_good, axis=1)
@@ -181,10 +185,11 @@ class VerbTensorEvaluator():
         if n_good > 5:
             logging.info(f'{target}\t{self.weight}\t{self.rank}\t{self.include_empty}\t{self.cutoff}\t{n_good}')
 
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)-8s [%(lineno)d] %(message)s')
     evalor = VerbTensorEvaluator(weight='niact', include_empty=False,
-            cutoff=1000000, rank=512)
+                                 cutoff=1000000, rank=512)
     evalor.test_sim(read_ks().reset_index())
-    #evalor.predict_verb(read_gs().reset_index())
+    # evalor.predict_verb(read_gs().reset_index())
